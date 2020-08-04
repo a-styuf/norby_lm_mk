a0ed8f39e2cf328f96ac271dba395b2fb7e8c415
a-styuf <a-styuf@yandex.ru>
2020-08-04 12:59:09 +0700

    # 1.06.2 Версия перед сборкой на клей

--- my_files/lm.h
+++ my_files/lm.h
@@ -29,7 +29,7 @@ typedef unsigned short uint16_t;
 
 // настройки прибора
 #define DEV_ID (0x06)
-#define SOFT_VERSION "1.06.1"
+#define SOFT_VERSION "1.06.2"
 // свойства микроконтроллера
 
 // раскрашивание переменных
