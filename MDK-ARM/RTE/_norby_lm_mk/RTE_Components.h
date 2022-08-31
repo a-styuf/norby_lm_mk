f84396882446d433897b0e8b286919836033043a
a-styuf <a-styuf@yandex.ru>
2022-08-31 09:38:57 +0700

    added aa_can libs and changed .gitignore

--- MDK-ARM/RTE/_norby_lm_mk/RTE_Components.h
+++ MDK-ARM/RTE/_norby_lm_mk/RTE_Components.h
@@ -1,30 +1,26 @@
-
-/*
- * Auto generated Run-Time-Environment Configuration File
- *      *** Do not modify ! ***
- *
- * Project: 'norby_lm_mk' 
- * Target:  'norby_lm_mk' 
- */
-
-#ifndef RTE_COMPONENTS_H
-#define RTE_COMPONENTS_H
-
-
-/*
- * Define the Device Header File: 
- */
-#define CMSIS_device_header "stm32f4xx.h"
-
-/* Keil.ARM Compiler::Compiler:I/O:STDERR:ITM:1.2.0 */
-#define RTE_Compiler_IO_STDERR          /* Compiler I/O: STDERR */
-          #define RTE_Compiler_IO_STDERR_ITM      /* Compiler I/O: STDERR ITM */
-/* Keil.ARM Compiler::Compiler:I/O:STDIN:ITM:1.2.0 */
-#define RTE_Compiler_IO_STDIN           /* Compiler I/O: STDIN */
-          #define RTE_Compiler_IO_STDIN_ITM       /* Compiler I/O: STDIN ITM */
-/* Keil.ARM Compiler::Compiler:I/O:STDOUT:ITM:1.2.0 */
-#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
-          #define RTE_Compiler_IO_STDOUT_ITM      /* Compiler I/O: STDOUT ITM */
-
-
-#endif /* RTE_COMPONENTS_H */
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'norby_lm_mk' 
+ * Target:  'norby_lm_mk' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+#define RTE_Compiler_IO_STDERR          /* Compiler I/O: STDERR */
+          #define RTE_Compiler_IO_STDERR_ITM      /* Compiler I/O: STDERR ITM */
+#define RTE_Compiler_IO_STDIN           /* Compiler I/O: STDIN */
+          #define RTE_Compiler_IO_STDIN_ITM       /* Compiler I/O: STDIN ITM */
+#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
+          #define RTE_Compiler_IO_STDOUT_ITM      /* Compiler I/O: STDOUT ITM */
+
+#endif /* RTE_COMPONENTS_H */
